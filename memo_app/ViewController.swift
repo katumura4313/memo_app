bd9d1a2cea6efc56765085c1305d5471f29f160f
katsumura4313 <n4ru4313@gmail.com>
2020-05-19 19:22:56 +0900

    プロジェクト作成

---
+++ memo_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  memo_app
+//
+//  Created by USER on 2020/05/19.
+//  Copyright © 2020 jp.go. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
